c49dc370ebd8fccd30968e19d580404ea57a49e5
Petr Stipek <pipaslot@seznam.cz>
2019-12-16 02:46:58 +0100

    added dummy api controller

---
+++ App.Server/Controllers/ValueController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Server.Controllers
+{
+    [Route("api/value")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetAll()
+        {
+            return new string[]
+            {
+                "AAAA",
+                "BBBB"
+            };
+        }
+    }
+}
