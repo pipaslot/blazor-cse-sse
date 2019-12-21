689e4729ff72f183fd8f162bb250bcef44400bfa
Petr Stipek <pipaslot@seznam.cz>
2019-12-21 22:44:39 +0100

    extend grid functionality

---
+++ Components/Shared/BColSize.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Components.Shared
+{
+    public static class BColSize
+    {
+        public const string All_1  = " 1";
+        public const string All_2  = " 2";
+        public const string All_3  = " 3";
+        public const string All_4  = " 4";
+        public const string All_5  = " 5";
+        public const string All_6  = " 6";
+        public const string All_7  = " 7";
+        public const string All_8  = " 8";
+        public const string All_9  = " 9";
+        public const string All_10 = " 10";
+        public const string All_11 = " 11";
+        public const string All_12 = " 12";
+
+        public const string XS_1  = " xs-1";
+        public const string XS_2  = " xs-2";
+        public const string XS_3  = " xs-3";
+        public const string XS_4  = " xs-4";
+        public const string XS_5  = " xs-5";
+        public const string XS_6  = " xs-6";
+        public const string XS_7  = " xs-7";
+        public const string XS_8  = " xs-8";
+        public const string XS_9  = " xs-9";
+        public const string XS_10 = " xs-10";
+        public const string XS_11 = " xs-11";
+        public const string XS_12 = " xs-12";
+        
+        public const string SM_1  = " sm-1";
+        public const string SM_2  = " sm-2";
+        public const string SM_3  = " sm-3";
+        public const string SM_4  = " sm-4";
+        public const string SM_5  = " sm-5";
+        public const string SM_6  = " sm-6";
+        public const string SM_7  = " sm-7";
+        public const string SM_8  = " sm-8";
+        public const string SM_9  = " sm-9";
+        public const string SM_10 = " sm-10";
+        public const string SM_11 = " sm-11";
+        public const string SM_12 = " sm-12";
+        
+        public const string MD_1  = " md-1";
+        public const string MD_2  = " md-2";
+        public const string MD_3  = " md-3";
+        public const string MD_4  = " md-4";
+        public const string MD_5  = " md-5";
+        public const string MD_6  = " md-6";
+        public const string MD_7  = " md-7";
+        public const string MD_8  = " md-8";
+        public const string MD_9  = " md-9";
+        public const string MD_10 = " md-10";
+        public const string MD_11 = " md-11";
+        public const string MD_12 = " md-12";
+        
+        public const string LG_1  = " LG-1";
+        public const string LG_2  = " LG-2";
+        public const string LG_3  = " LG-3";
+        public const string LG_4  = " LG-4";
+        public const string LG_5  = " LG-5";
+        public const string LG_6  = " LG-6";
+        public const string LG_7  = " LG-7";
+        public const string LG_8  = " LG-8";
+        public const string LG_9  = " LG-9";
+        public const string LG_10 = " LG-10";
+        public const string LG_11 = " LG-11";
+        public const string LG_12 = " LG-12";
+        
+        public const string XL_1  = " xl-1";
+        public const string XL_2  = " xl-2";
+        public const string XL_3  = " xl-3";
+        public const string XL_4  = " xl-4";
+        public const string XL_5  = " xl-5";
+        public const string XL_6  = " xl-6";
+        public const string XL_7  = " xl-7";
+        public const string XL_8  = " xl-8";
+        public const string XL_9  = " xl-9";
+        public const string XL_10 = " xl-10";
+        public const string XL_11 = " xl-11";
+        public const string XL_12 = " xl-12";
+
+    }
+}
---
+++ Components/Shared/BRowAlign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Components.Shared
+{
+    public static class BRowAlign
+    {
+        public const string Start = "start";
+        public const string Center = "center";
+        public const string End = "end";
+    }
+}
