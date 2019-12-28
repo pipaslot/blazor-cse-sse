6606c4314e7f7979d983f146f54e76ca98770ce2
Petr Stipek <pipaslot@seznam.cz>
2019-12-29 00:57:03 +0100

    add server side localisation

--- Components/States/AppState.cs
+++ Components/States/AppState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 using Cloudcrate.AspNetCore.Blazor.Browser.Storage;
@@ -16,8 +17,22 @@ namespace Components.States
         {
             get => Data.Language;
             set { Data.Language = value;
+                SwitchLanguage(value);
                 SaveChanges();
             }
+        }
+
+        private void SwitchLanguage(string language)
+        {
+            var culture = new CultureInfo(language);
+            //CultureInfo.CurrentCulture = culture;
+            Resources.Layout.Culture = culture;
+        }
+
+        protected override async Task OnLoad()
+        {
+            await base.OnLoad();
+            SwitchLanguage(Language);
         }
     }
     public class AppStateData
