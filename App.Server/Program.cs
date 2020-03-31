7d31742b23237e503ec6d27d1ff60cb1a75223cd
Petr Stipek <pipaslot@seznam.cz>
2020-03-31 23:05:00 +0200

    change program file definition

--- App.Server/Program.cs
+++ App.Server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Web;
 using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
 
 namespace App.Server
 {
@@ -10,41 +12,18 @@ namespace App.Server
     {
         public static void Main(string[] args)
         {
-            // NLog: setup the logger first to catch all errors
-            NLog.Logger logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-
-            try
-            {
-                logger.Debug("______________________________________________________________________");
-                logger.Debug("Building Host in Main()");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                //NLog: catch setup errors
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                logger.Debug("Shutting down NLOG");
-                NLog.LogManager.Shutdown();
-            }
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                    {
-                        logging.ClearProviders();
-                        logging.SetMinimumLevel(LogLevel.Trace);
-                    })
-                .UseNLog()  // NLog: setup NLog for Dependency injection
-                .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>();
-                    });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build())
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
     }
 }
