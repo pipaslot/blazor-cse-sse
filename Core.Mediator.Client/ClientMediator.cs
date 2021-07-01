4027f95901e8a476d86c872221e67df35fcdebf6
Petr Stipek <petr.stipek@bitbee.ch>
2021-07-01 12:31:29 +0200

    clean

--- Core.Mediator.Client/ClientMediator.cs
+++ Core.Mediator.Client/ClientMediator.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
